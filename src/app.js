4d5dfdced077158d665c115c85f32e5e45f0dcfc
CARTeeyuh <ethanpua10@gmail.com>
2026-03-04 14:44:39 -0800

    Started on the mobile part

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
-import { Chart } from "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.5.0/chart.min.js"
+import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js@4.5.1/+esm'
 const graph = document.querySelector("#graph")
 const chart = new Chart(graph, {
     type: "pie", data: {
         labels: []
     }
 })
+
+const nav_header = document.querySelector("nav>h1");
+let current_section = 0;
+const sections = document.querySelectorAll("section");
+console.log(2);
+if (window.matchMedia("screen and (max-width: 600px)").matches) {
+    console.log(1);
+    nav_header.textContent = sections.item(current_section).firstElementChild.textContent;
+}
