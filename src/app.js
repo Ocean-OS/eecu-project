703ea470d9316df59e2373fa5eff5cc53ef0e000
JakeFaridoni <jafitup@outlook.com>
2026-03-05 14:58:44 -0800

    Restructure HTML, improve mobile compat (#6)

--- src/app.js
+++ src/app.js
@@ -1,16 +1,31 @@
 import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js@4.5.1/+esm'
 const graph = document.querySelector("#graph")
-const chart = new Chart(graph, {
-    type: "pie", data: {
-        labels: []
-    }
-})
+const next = document.querySelector('#next-btn');
+const back = document.querySelector('#back-btn');
+// const chart = new Chart(graph, {
+//     type: "pie", data: {
+//         labels: []
+//     }
+// })
 
 const nav_header = document.querySelector("nav>h1");
 let current_section = 0;
 const sections = document.querySelectorAll("section");
 console.log(2);
+window.matchMedia("screen and (max-width: 600px)").addEventListener("change", (event) => {
+    if (event.matches) {
+        nav_header.textContent = sections.item(current_section).firstElementChild.textContent;
+        console.log(nav_header);
+    } else {
+        nav_header.textContent = "Budget Calculator";
+    }
+
+});
 if (window.matchMedia("screen and (max-width: 600px)").matches) {
-    console.log(1);
     nav_header.textContent = sections.item(current_section).firstElementChild.textContent;
+    console.log(1);
 }
+
+next.addEventListener("click", () => {
+    
+});
