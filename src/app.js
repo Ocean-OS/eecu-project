20d3b8e8e0414324ba9338de44b8700f9958f8a0
ComputerGuy <63362464+Ocean-OS@users.noreply.github.com>
2026-03-12 15:51:20 -0700

    add stuff

--- src/app.js
+++ src/app.js
@@ -1,28 +1,42 @@
-import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js@4.5.1/+esm'
-const graph = document.querySelector("#graph")
+// @ts-check
+// @ts-expect-error
+import { Chart } from 'https://cdn.jsdelivr.net/npm/chart.js@4.5.1/+esm';
+// @ts-expect-error
+import { compile } from 'https://dy.github.io/watr/watr.js';
+const graph = document.querySelector('#graph');
 const next = document.querySelector('#next-btn');
 const back = document.querySelector('#back-btn');
+const text = await fetch('./app.wat').then(res => res.text());
+const compiled = await WebAssembly.compile(compile(text));
+/** @type {Map<number, any>} */
+const objects = new Map();
 
-async function careerOptions () {
+async function careerOptions() {
     const selectCareer = document.getElementById('career-inputs');
     const careerAnnualSalary = new Map();
+    const salary = selectCareer?.nextElementSibling?.firstElementChild;
     try {
-        const response = await fetch ('https://eecu-data-server.vercel.app/data');
+        const response = await fetch(
+            'https://eecu-data-server.vercel.app/data'
+        );
         if (!response.ok) {
-            throw new Error ('Network response was not ok');
+            throw new Error('Network response was not ok');
         }
 
-        const users = await response.json();
+        const users = /** @type {Array<Record<String, number | string>>} */ (
+            await response.json()
+        );
 
         users.forEach(user => {
-            careerAnnualSalary.set(user["Occupation"], user["Salary"]);
-            const option = new Option(user["Occupation"], user["Occupation"]);
-            selectCareer.add(option);
+            careerAnnualSalary.set(user['Occupation'], user['Salary']);
+            const option = new Option(user['Occupation'], user['Occupation']);
+            selectCareer?.add(option);
         });
 
-        selectCareer.addEventListener('change', () => {
-            salary.textContent = careerAnnualSalary.get(selectCareer.value) || '';
-        })
+        selectCareer?.addEventListener('change', () => {
+            salary.value =
+                careerAnnualSalary.get(selectCareer.value) || '';
+        });
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
     }
@@ -30,16 +44,190 @@ async function careerOptions () {
 
 careerOptions();
 
-const compiled = await WebAssembly.compileStreaming(await fetch('data:application/wasm;base64,AGFzbQEAAAABDwNefwFgAX8Bf2ACf38BfwIPAQNlbnYHc3lzY2FsbAABAwIBAgUEAQCACAYMAWQAAUEAQQD7BgALBxACBm1lbW9yeQIAA3N1bQABCl4BXAIBZAADfyABQQD7BwAhAgNAIAQgAGooAgAhBSACIAQgBfsOACAEQQFqIQQgBCABTA0AC0EAIQNBACEEA0AgAyACIAT7CwBqIQMgBEEBaiEEIAQgAUwNAAsgAw8L'));
-const { exports: { sum } } = await WebAssembly.instantiate(compiled, {
-    env: {
-        syscall() {
+let freed = 0;
 
+/**
+ * @param {number} ptr
+ */
+function find_until_null(ptr) {
+    const buffer = new Uint8Array(
+        /** @type {WebAssembly.Memory} */ (memory).buffer.slice(ptr)
+    );
+    let i = 0;
+    while (i < buffer.byteLength) {
+        if (buffer[i++] === 0) {
+            return i - 1;
+        }
+    }
+}
+
+const {
+    exports: { sum, sum_category, memory: _memory }
+} = await WebAssembly.instantiate(compiled, {
+    env: {
+        /**
+         * @param {number} ptr_to_selector
+         */
+        'document.querySelector': ptr_to_selector => {
+            const string = new TextDecoder().decode(
+                /** @type {WebAssembly.Memory} */ (memory).buffer.slice(
+                    ptr_to_selector,
+                    find_until_null(ptr_to_selector)
+                )
+            );
+            const res = document.querySelector(string);
+            if (res === null) {
+                objects.set(objects.size, null);
+                return objects.size - 1;
+            }
+            objects.set(objects.size, new WeakRef(res));
+            return objects.size - 1;
+        },
+        /**
+         * @param {number} target
+         * @param {number} property
+         */
+        'Reflect.get': (target, property) => {
+            const object = objects.get(target);
+            if (object === null || object === undefined) {
+                throw new TypeError('Cannot read properties of null');
+            }
+            const string = new TextDecoder().decode(
+                /** @type {WebAssembly.Memory} */ (memory).buffer.slice(
+                    property,
+                    find_until_null(property)
+                )
+            );
+            const value = Reflect.get(object, string);
+            if (value === null) {
+                objects.set(objects.size, null);
+                return objects.size - 1;
+            }
+            objects.set(objects.size, new WeakRef(value));
+            return objects.size - 1;
+        },
+        'Reflect.set': (
+            /** @type {number} */ target,
+            /** @type {number} */ property,
+            /** @type {number} */ value
+        ) => {
+            const object = objects.get(target);
+            const key = new TextDecoder().decode(
+                /** @type {WebAssembly.Memory} */ (memory).buffer.slice(
+                    property,
+                    find_until_null(property)
+                )
+            );
+            const v = objects.has(value) ? objects.get(value) : value;
+            Reflect.set(object, key, v);
+        },
+        /**
+         * @param {number} ptr_to_selector
+         */
+        'document.querySelectorAll': ptr_to_selector => {
+            const string = new TextDecoder().decode(
+                /** @type {WebAssembly.Memory} */ (memory).buffer.slice(
+                    ptr_to_selector,
+                    find_until_null(ptr_to_selector)
+                )
+            );
+            const res = document.querySelectorAll(string);
+            const ptr = objects.size;
+            for (const element of res) {
+                objects.set(objects.size, element);
+            }
+            return ptr;
+        },
+        localStorage: (objects.set(objects.size, localStorage), objects.size - 1),
+        /**
+         * @param {number} object
+         */
+        'JSON.stringify': object => {
+            const value = objects.get(object);
+            const string = JSON.stringify(value);
+            const u8array = new Uint8Array(memory.buffer);
+            const ptr = freed;
+            for (let i = 0; i < string.length; i++) {
+                u8array[freed++] = string.charCodeAt(i);
+            }
+            return ptr;
+        },
+        /**
+         * @param {number} string
+         */
+        'JSON.parse': string => {
+            const str = new TextDecoder().decode(
+                /** @type {WebAssembly.Memory} */ (memory).buffer.slice(
+                    string,
+                    find_until_null(string)
+                )
+            );
+            const value = JSON.parse(str);
+            objects.set(objects.size, value);
+            return objects.size - 1;
+        },
+        'Object.create': () => {
+            objects.set(objects.size, {});
+            return objects.size - 1;
         }
     }
 });
 
-console.log(sum([1, 2, 3]));
+const memory = /** @type {WebAssembly.Memory} */ (_memory);
+
+/**
+ * @param {number[]} nums
+ */
+function wasm_sum(nums) {
+    const int32array = new Int32Array(
+        /** @type {WebAssembly.Memory} */ (memory).buffer
+    );
+    const ptr = freed;
+    for (let i = 0; i < nums.length; i++) {
+        int32array[freed++] = nums[i];
+    }
+    const res = sum(ptr, (nums.length - 1) * 16);
+    // for (let i = 0; i < nums.length; i++) {
+    //     int32array[--freed] = 0;
+    // }
+    return res;
+}
+
+/**
+ * @param {Array<{ yearly: boolean; value: number }>} inputs
+ */
+function wasm_sum_category(inputs) {
+    const f64array = new Float64Array(
+        /** @type {WebAssembly.Memory} */ (memory).buffer
+    );
+    const ptr = freed;
+    for (let i = 0; i < inputs.length; i++) {
+        const input = inputs[i];
+        f64array[freed++] = input.value;
+        f64array[freed++] = input.yearly ? 1 : 0;
+    }
+    const res = sum_category(ptr, (inputs.length - 1) * 32);
+    // for (let i = 0; i < inputs.length; i++) {
+    //     f64array[--freed] = f64array[--freed] = 0;
+    // }
+    return res;
+}
+
+console.log(memory);
+console.log(wasm_sum([1, 2, 3, 4, 5, 6]));
+console.log(
+    wasm_sum_category([
+        {
+            value: 500,
+            yearly: true
+        },
+        {
+            value: 50,
+            yearly: false
+        }
+    ])
+);
+console.log();
 
 // const chart = new Chart(graph, {
 //     type: "pie", data: {
@@ -50,13 +238,18 @@ console.log(sum([1, 2, 3]));
 // //getting the data
 const data = {
     labels: ['Orange', 'Red', 'Yellow'],
-    datasets: [{
-        label: 'dataset:',
-        data: [40, 30, 30], // then put them as percentage
-        backgroundColor: ['rgba(255, 139, 82, 1)', 'rgba(233, 52, 52, 1)', 'rgba(241, 217, 81, 1)']
-    }]
-}
-
+    datasets: [
+        {
+            label: 'dataset:',
+            data: [40, 30, 30], // then put them as percentage
+            backgroundColor: [
+                'rgba(255, 139, 82, 1)',
+                'rgba(233, 52, 52, 1)',
+                'rgba(241, 217, 81, 1)'
+            ]
+        }
+    ]
+};
 
 // // how graph is created with prior code
 // const context = document.getElementById('graph');
@@ -66,51 +259,54 @@ const data = {
 //     options: // name of options
 // });
 
-
-const nav_header = document.querySelector("nav>h1");
+const nav_header = document.querySelector('nav>h1');
 let current_section = 0;
-const sections = document.querySelectorAll("section");
+const sections = document.querySelectorAll('section');
 console.log(2);
-window.matchMedia("screen and (max-width: 600px)").addEventListener("change", (event) => {
-    if (event.matches) {
-        nav_header.textContent = sections.item(current_section).firstElementChild.textContent;
+window
+    .matchMedia('screen and (max-width: 600px)')
+    .addEventListener('change', event => {
+        if (event.matches) {
+            nav_header.textContent =
+                sections.item(current_section)?.firstElementChild?.textContent;
+        } else {
+            nav_header.textContent = 'Budget Calculator';
+        }
+    });
 
-    } else {
-        nav_header.textContent = "Budget Calculator";
-    }
-})
-
-if (window.matchMedia("screen and (max-width: 600px)").matches) {
+if (window.matchMedia('screen and (max-width: 600px)').matches) {
     console.log(1);
-    nav_header.textContent = sections.item(current_section).firstElementChild.textContent;
+    nav_header.textContent =
+        sections.item(current_section)?.firstElementChild?.textContent;
 }
 
-next.addEventListener("click", () => {
+next?.addEventListener('click', () => {
     navigate(current_section + 1);
-
 });
 
-back.addEventListener("click", () => {
+back?.addEventListener('click', () => {
     navigate(current_section - 1);
-
 });
 
+/**
+ * @param {number} page
+ */
 function navigate(page) {
     if (page == current_section || page < 0 || page > sections.length - 1) {
         return;
-
     }
     console.log(sections, current_section);
-    sections.item(current_section).classList.remove("active");
-    sections.item(current_section).classList.add("mobile-inactive");
-    const section = sections.item(current_section = page);
-    section.classList.add("active");
-    section.classList.remove("mobile-inactive");
-    nav_header.textContent = sections.item(current_section).firstElementChild.textContent;
-
+    sections.item(current_section).classList.remove('active');
+    sections.item(current_section).classList.add('mobile-inactive');
+    const section = sections.item((current_section = page));
+    section.classList.add('active');
+    section.classList.remove('mobile-inactive');
+    nav_header.textContent =
+        sections.item(current_section)?.firstElementChild?.textContent;
 }
 let current_chart;
-const chart_container = () => document.querySelector("#graph").getContext("2d");
+const chart_container = () =>
+    document.querySelector('#graph')?.getContext('2d');
 
 /*
 function updatePieChart() {
@@ -141,3 +337,14 @@ function updatePieChart() {
 }
 
 updatePieChart() */
+
+//     }
+//     console.log(sections, current_section);
+//     sections.item(current_section).classList.remove('active');
+//     sections.item(current_section).classList.add('mobile-inactive');
+//     const section = sections.item((current_section = page));
+//     section.classList.add('active');
+//     section.classList.remove('mobile-inactive');
+//     nav_header.textContent =
+//         sections.item(current_section).firstElementChild.textContent;
+// }
--- src/app.wat
+++ src/app.wat
@@ -1,77 +1,66 @@
 (module
-    (import "env" "syscall" (func $syscall (param i32) (result i32)))
-    (memory (export "memory") 1024)
-    (type $Numbers (array (mut i32)))
-    (global $inputs (mut (ref $Numbers))
-        (array.new $Numbers
-            (i32.const 0)
-            (i32.const 0)
+    (import "env" "document.querySelector" (func $document.querySelector (param i32
+            ) (result i32)
         )
     )
+    (import "env" "document.querySelectorAll" (func $document.querySelectorAll (param i32
+            ) (result i32)
+        )
+    )
+    (import "env" "Reflect.get" (func $Reflect.get (param i32 i32) (result i32))
+    )
+    (import "env" "Reflect.set" (func $Reflect.set (param i32 i32 i32)))
+    (import "env" "Object.create" (func $Object.create (result i32)))
+    (import "env" "JSON.stringify" (func $JSON.stringify (param i32) (result i32
+            )
+        )
+    )
+    (import "env" "JSON.parse" (func $JSON.parse (param i32) (result i32)))
+    (import "env" "localStorage" (global $localStorage i32))
+    (memory (export "memory") 2048)
+    (data $input_selector
+        (i32.const 1024) ".current-page > input[type=number]"
+    )
+    (data $select_selector
+        (i32.const 1152) ".current-page > select"
+    )
+    (data $value
+        (i32.const 1280) "value"
+    )
+    (data $localstorage_key
+        (i32.const 1408) "data"
+    )
     (func $sum (export "sum") (param $ptr i32) (param $len i32) (result i32)
-        (local $nums (ref $Numbers))
         (local $res i32)
         (local $i i32)
-        (local $j i32)
-        (local.set $nums
-            (array.new_default $Numbers
-                (local.get $len)
-                (i32.const 0)
-            )
-        )
-        (loop $apply
-            (local.set $j
-                (i32.load
-                    (i32.add
-                        (local.get $i)
-                        (local.get $ptr)
-                    )
-                )
-            )
-            (array.set $Numbers
-                (local.get $nums)
-                (local.get $i)
-                (local.get $j)
-            )
-            (local.set $i
-                (i32.add
-                    (local.get $i)
-                    (i32.const 1)
-                )
-            )
-            (br_if $apply
-                (i32.le_s
-                    (local.get $i)
-                    (local.get $len)
-                )
-            )
-        )
         (local.set $res
             (i32.const 0)
         )
         (local.set $i
-            (i32.const 0)
+            (local.get $ptr)
         )
         (loop $loop
             (local.set $res
                 (i32.add
                     (local.get $res)
-                    (array.get $Numbers
-                        (local.get $nums)
-                        (local.get $i)
+                    (i32.load
+                        (local.get $ptr)
                     )
                 )
             )
-            (local.set $i
+            (local.set $ptr
                 (i32.add
-                    (local.get $i)
-                    (i32.const 1)
+                    (local.get $ptr)
+                    (i32.const 4)
                 )
             )
             (br_if $loop
                 (i32.le_s
-                    (local.get $i)
-                    (local.get $len)
+                    (local.get $ptr)
+                    (i32.add
+                        (local.get $len)
+                        (local.get $i)
+                    )
                 )
             )
         )
@@ -79,4 +68,91 @@
             (local.get $res)
         )
     )
+    (func $sum_category (export "sum_category") (param $ptr i32) (param $len i32
+        ) (result f64)
+        (local $res f64)
+        (local $i f64)
+        (local $curr i32)
+        (local.set $curr
+            (local.get $ptr)
+        )
+        (loop $sum
+            (local.set $i
+                (f64.load
+                    (local.get $curr)
+                )
+            )
+            ;; if the value is meant to be a yearly amount, divide by 12 to get the monthly rate
+            (if
+                (f64.eq
+                    (f64.load
+                        (i32.add
+                            (local.get $curr)
+                            (i32.const 8)
+                        )
+                    )
+                    (f64.const 1)
+                )
+                (then
+                    (local.set $i
+                        (f64.div
+                            (local.get $i)
+                            (f64.const 12)
+                        )
+                    )
+                )
+            )
+            (local.set $res
+                (f64.add
+                    (local.get $res)
+                    (local.get $i)
+                )
+            )
+            ;; basic pointer arithmetic - a float64 is 8 bytes, so two float64s are 16 bytes
+            (local.set $curr
+                (i32.add
+                    (local.get $curr)
+                    (i32.const 16)
+                )
+            )
+            (br_if $sum
+                (i32.le_s
+                    (local.get $curr)
+                    (i32.add
+                        (local.get $ptr)
+                        (local.get $len)
+                    )
+                )
+            )
+        )
+        ;; truncate value to 2 decimal places
+        (local.set $res
+            (f64.mul
+                (local.get $res)
+                (f64.const 100)
+            )
+        )
+        (local.set $res
+            (f64.trunc
+                (local.get $res)
+            )
+        )
+        (local.set $res
+            (f64.div
+                (local.get $res)
+                (f64.const 100)
+            )
+        )
+        (return
+            (local.get $res)
+        )
+    )
+    ;; (func $calculate (export "calculate") (result f64)
+    ;;     (local $inputs i32)
+    ;;     (local $input_values i32)
+    ;;     (local $selects i32)
+    ;;     (local.set $inputs (call $document.querySelectorAll (i32.const 64)))
+    ;;     (local.set $selects (call $document.querySelectorAll (i32.const 128)))
+
+    ;; )
 )
